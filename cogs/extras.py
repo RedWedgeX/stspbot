def2ed221003468ae7c1f50843c827f878cff5e3
redwedgex <redwedgex@protonmail.com>
2022-01-24 11:42:17 -0800

    vc script bot plays once and stops

--- cogs/extras.py
+++ cogs/extras.py
@@ -11,20 +11,19 @@ class Extras(commands.Cog, name="Stuff for funzies"):
     @commands.command(hidden=True)
     @commands.has_any_role(staff)
     async def script(self, ctx, movie):
-        while True:
-            try:
-                await ctx.voice_client.disconnect()
+        try:
+            await ctx.voice_client.disconnect()
 
-                chanid=  918285694122721351
-                chan = self.bot.get_channel(int(chanid))
-                await ctx.send(f"connecting to {chan.name}")
-                vc = await chan.connect()
+            chanid=  918285694122721351
+            chan = self.bot.get_channel(int(chanid))
+            await ctx.send(f"connecting to {chan.name}")
+            vc = await chan.connect()
 
-                vc.play(discord.FFmpegPCMAudio(f'scripts/{movie}.mp3'))
-            except Exception as e:
-                await ctx.send(f'```py\n{traceback.format_exc()}\n```')
-            else:
-                await ctx.send
+            vc.play(discord.FFmpegPCMAudio(f'scripts/{movie}.mp3'))
+        except Exception as e:
+            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+        else:
+            await ctx.send
 
 def setup(client):
     client.add_cog(Extras(client))
